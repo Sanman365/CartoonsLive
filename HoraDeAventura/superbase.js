18c9d10cd6eb0220cbdd210a70fa3f46a32f3d5b
Sanman365 <ariel2906to@gmail.com>
2025-03-05 20:08:36 -0600

    Update superbase.js

--- HoraDeAventura/superbase.js
+++ HoraDeAventura/superbase.js
@@ -1,9 +1,9 @@
-
 // Importa el SDK de Supabase desde un CDN
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const supabaseUrl = 'https://ihxhquvjopmpgjysugqw.supabase.co';
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloeGhxdXZqb3BtcGdqeXN1Z3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1OTgyNDIsImV4cCI6MjA1NjE3NDI0Mn0.G6NZ15PSg0U4tZ0CJwjzEud3ns94usGgQ3rUp7VCDp0;
+// Configura Supabase con tus credenciales
+const supabaseUrl = 'https://ihxhquvjopmpgjysugqw.supabase.co'; // Tu URL de Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloeGhxdXZqb3BtcGdqeXN1Z3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1OTgyNDIsImV4cCI6MjA1NjE3NDI0Mn0.G6NZ15PSg0U4tZ0CJwjzEud3ns94usGgQ3rUp7VCDp0'; // Tu clave anónima
 const supabase = supabase.createClient(supabaseUrl, supabaseKey)
 
 function enviarReporte(tipo) {
